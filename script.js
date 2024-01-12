b843b72031947fd8f7c795f943815b418f34ddbf
Dverybest <duio1541@gmail.com>
2024-01-12 18:51:53 -0500

    My first commit

---
+++ script.js
@@ -0,0 +1,259 @@
+// alert("Welcome to 2024 Calender")
+
+const ny = document.querySelector("#ny")
+
+const time =()=>{
+    setInterval(() => {
+     console.log("Happy New year");
+    
+    }, 10000);
+}
+
+ny.addEventListener("click", ()=>{
+    ny.innerHTML= "Happy New Year";
+},
+ny.style.color="green")
+
+const lb = document.querySelector("#lb")
+
+const lbd =()=>{
+    setInterval(() => {
+     console.log("Labour Day");
+    
+    }, 10000);
+}
+
+lb.addEventListener("click", ()=>{
+    lb.innerHTML= "Labour Day";
+},
+lb.style.color="green")
+
+
+const cd = document.querySelector("#cd")
+
+const csd =()=>{
+    setInterval(() => {
+     console.log("Happy Children's Day");
+    
+    }, 10000);
+}
+
+cd.addEventListener("click", ()=>{
+    cd.innerHTML= "Happy Children's Day";
+},
+cd.style.color="green")
+
+
+const bxd = document.querySelector("#bxd")
+
+const bx =()=>{
+    setInterval(() => {
+     console.log("Happy Boxing Day");
+    
+    }, 10000);
+}
+
+bxd.addEventListener("click", ()=>{
+    bxd.innerHTML= "Happy Boxing Day";
+},
+bxd.style.color="green")
+
+
+const dd = document.querySelector("#dd")
+
+const dem =()=>{
+    setInterval(() => {
+     console.log("Happy Democracy");
+    
+    }, 10000);
+}
+
+dd.addEventListener("click", ()=>{
+    dd.innerHTML= "Happy Democracy";
+},
+dd.style.color="green")
+
+
+
+
+
+const vl = document.querySelector("#vl")
+
+const dime =()=>{
+    setInterval(() => {
+     console.log("St.Valentine's Day");
+    
+    }, 10000);
+}
+
+vl.addEventListener("mouseover", ()=>{
+    vl.innerHTML = "St.Valentine's Day"
+},
+vl.style.color="Red")
+
+
+const af = document.querySelector("#af")
+
+const timee =()=>{
+    setInterval(() => {
+     console.log("April Fool");
+    
+    }, 10000);
+}
+
+af.addEventListener("click", ()=>{
+    af.innerHTML= "April Fool";
+},
+af.style.color="Navy")
+
+
+const mb = document.querySelector("#mb")
+
+const lime =()=>{
+    setInterval(() => {
+     console.log("Happy Birthday");
+    
+    }, 10000);
+}
+
+mb.addEventListener("mouseover", ()=>{
+    mb.innerHTML = "Happy Birthday To Me"
+},
+mb.style.color="yellow")
+
+const ud = document.querySelector("#ud")
+
+const udo =()=>{
+    setInterval(() => {
+     console.log("Happy Birthday UD");
+    
+    }, 10000);
+}
+
+ud.addEventListener("mouseover", ()=>{
+    ud.innerHTML = "Happy Birthday UD"
+},
+ud.style.color="cyan")
+
+
+const mom = document.querySelector("#mom")
+
+const ma =()=>{
+    setInterval(() => {
+     console.log("Happy Birthday Mom");
+    
+    }, 10000);
+}
+
+mom.addEventListener("mouseover", ()=>{
+    mom.innerHTML = "Happy Birthday Mum"
+},
+mom.style.color="cyan")
+
+
+const sb = document.querySelector("#sb")
+
+const Sexy =()=>{
+    setInterval(() => {
+     console.log("Happy Birthday Sexy");
+    
+    }, 10000);
+}
+sb.addEventListener("mouseover", ()=>{
+    sb.innerHTML = "Happy Birthday Sexy"
+},
+sb.style.color="cyan")
+
+
+const idy = document.querySelector("#idy")
+
+const nima =()=>{
+    setInterval(() => {
+     console.log("Happy Independence day");
+    
+    }, 10000);
+}
+
+idy.addEventListener("mouseover", ()=>{
+    idy.innerHTML = "Happy Independence Day"
+},
+idy.style.color="green")
+
+const db = document.querySelector("#db")
+
+const sima =()=>{
+    setInterval(() => {
+     console.log("Happy Birthday Dad");
+    
+    }, 10000);
+}
+
+db.addEventListener("mouseover", ()=>{
+    db.innerHTML = "Happy Birthday Dad"
+},
+db.style.color="cyan")
+
+const cb = document.querySelector("#cb")
+
+const lira =()=>{
+    setInterval(() => {
+     console.log("Happy Birthday Chima");
+    
+    }, 10000);
+}
+
+cb.addEventListener("mouseover", ()=>{
+    cb.innerHTML = "Happy Birthday Chima"
+},
+cb.style.color="cyan")
+
+
+const nb = document.querySelector("#nb")
+
+const lima =()=>{
+    setInterval(() => {
+     console.log("Happy Birthday Neche");
+    
+    }, 10000);
+}
+
+nb.addEventListener("mouseover", ()=>{
+    nb.innerHTML = "Happy Birthday Neche "
+},
+nb.style.color="cyan")
+
+const kb = document.querySelector("#kb")
+
+const limo =()=>{
+    setInterval(() => {
+     console.log("Happy Birthday Kacy");
+    
+    }, 10000);
+}
+
+kb.addEventListener("mouseover", ()=>{
+    kb.innerHTML = "Happy Birthday Kacy"
+},
+kb.style.color="cyan")
+
+
+
+const boc = document.querySelector("#boc")
+
+const limea =()=>{
+    setInterval(() => {
+     console.log("Happy Birthday");
+    
+    }, 10000);
+}
+
+boc.addEventListener("mouseover", ()=>{
+    boc.innerHTML = "Merry Christmas"
+},
+boc.style.color="royalblue")
+
+const btn = document.querySelector("#btn");
+
+btn.addEventListener("click", function() {
+  window.location.href = "third.html";
+});
